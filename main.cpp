769145f8f254cd23c1aa78e89b4b357ff4cb27ff
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2025-10-16 12:33:23 -0400

    infinite loop

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 
-void addToDo(); 
-void popToDo(); 
-void displayList(); 
+void addToDo();
+void popToDo();
+void displayList();
 
 vector<todo> toDoList; /* FIFO queue of TODOs */
 
@@ -14,63 +14,64 @@ vector<todo> toDoList; /* FIFO queue of TODOs */
 int main(void) {
 
     int keepgoing = 1;
-    
-    
-    
-    
+
+
+
+
 
     while (keepgoing) {
-        int option; 
-        
-        displayList(); 
+        int option;
+
+        displayList();
         cout << "What would you like to do?" << endl;
         cout << "(1) Add a to-do to the list" << endl;
         cout << "(2) Remove a to-do from the list" << endl;
         cout << "(3) Exit " << endl;
-        
-        cout << "Choice: "; 
-        cin >> option; 
-        
+
+        cout << "Choice: ";
+        cin >> option;
+
         switch(option) {
             case 1:
-                addToDo(); 
+                addToDo();
             break;
             case 2:
                 popToDo();
             break;
             case 3:
-                cout << "See you later!" << endl; 
-                exit(0); 
+                cout << "See you later!" << endl;
+                keepgoing = 0;
             break;
-            default: 
+            default:
+                cout << "This choice does not exist yet" << endl;
         }
     }
 }
 
 void displayList() {
-    cout << "****** TO-DO LIST ******" << endl; 
+    cout << "****** TO-DO LIST ******" << endl;
     for (int i = 0; i < toDoList.size(); i++) {
-        cout << "* (" << i << ")" << toDoList.at(i) << endl; 
+        cout << "* (" << i << ")" << toDoList.at(i) << endl;
     }
     cout << "****** END LIST ********" << endl;
 }
 
 void addToDo() {
-    string content; 
-    
-    cout << "What would you like to put on the list?" << endl; 
-    cin >> content; 
-    
-    todo newToDo = {toDoList.size(), content}; 
-    
-    toDoList.push_back(newToDo); 
-    
-    cout << "New reminder added to the list!" << endl; 
+    string content;
+
+    cout << "What would you like to put on the list?" << endl;
+    cin >> content;
+
+    todo newToDo = {toDoList.size(), content};
+
+    toDoList.push_back(newToDo);
+
+    cout << "New reminder added to the list!" << endl;
 }
 
 void popToDo() {
     cout << "Removing earliest to-do from the list..." << endl;
-    toDoList.pop_back(); 
-    
-    cout << "Earliest to-do removed!" << endl; 
+    toDoList.pop_back();
+
+    cout << "Earliest to-do removed!" << endl;
 }
